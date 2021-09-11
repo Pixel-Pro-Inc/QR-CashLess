8321e4464660f46aaf7a5e296fec4be0ae6c6150
Yewo Theu <yewotheu123456789@gmail.com>
2021-09-11 20:28:36 +0200

    Rest API Skeleton

---
+++ API/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MenuController> _logger;
+
+        public MenuController(ILogger<MenuController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{info}")]
+        public string Get(string info) //store id as table number
+        {
+            return info;
+        }
+    }
+}
