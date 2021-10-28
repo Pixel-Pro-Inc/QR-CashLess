3afb02e5fa7cf39584e417a0d3807707284162a7
Abel Tshimbalanga <apexmachine2@gmail.com>
2021-10-28 14:51:28 +0200

    -I installed firesharp and Im not sure it helped in any real way. I am failing to test  cause the same error for the last time has, appeared, or at leasat a variation. apparently running scripts has been disabled, them execution policies

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Entities;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +14,46 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : Controller
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "KIxlMLOIsiqVrQmM0V7pppI1Ao67UPZv5jOdU0QJ",
+            BasePath = "https://rodizoapp-default-rtdb.firebaseio.com/",
+
+        };
+        IFirebaseClient firebaseClient;
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult PostNewData(AppUser user)
+        {
+            try
+            {
+                SendDataToDatabase(user);
+                ModelState.AddModelError(string.Empty, "Added successfully");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                throw;
+            }
+            return View();
+        }
+
+        
+         private void SendDataToDatabase(AppUser user)
+         {
+            firebaseClient = new FireSharp.FirebaseClient(config);
+            PushResponse response = firebaseClient.Push("Info/", user);
+            user.UserName = response.Result.name;
+            SetResponse setResponse = firebaseClient.Set("Info/" + user.UserName, user);
+         }
+        
+
     }
+   
 }
---
+++ API/Entities/RestBranch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class RestBranch
+    {
+        int BranchId;
+        string restaurantName;
+        string branchName;
+        string managerName;
+        string location;
+    }
+}
