0a4d79d62f45094ad3a648b5420cdbb1c8d465ba
Abel3047 <apexmachine2@gmail.com>
2022-05-11 14:15:21 +0200

    This is scafolding for the Reform due
    -We are considering using Clean architeture in the Rodizio Express
    project. To do so, one of the key move is to have our 'Core' app, (ie
    the domain logic and application logic) in a separate nuget package that
    all solutions will reference. Each solution will then have its own
    Infrastructure and Presentation project and test projects that will be
    maintained individually.
    -This was just the first step. I very much anticipate making this in a
    completely different solution file instead of in RodizioExpress so that
    it will be easier to refer to and maintain in the future.

---
+++ RodizioExpressCore/src/Domain/Model/Enums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class Enums
+    {
+        public enum restuarant
+        {
+            rodizio
+        }
+        /*
+         So I don't want to use several bools to have for the different things I am putting below, it just made more sense
+        to have an enum and have it store either a string or a number that would refer to each other. If enums don't work
+        we can decide to use a small dictionary so we don't have to work too much. Cause bools are easy, but hard to manage
+        practically
+         */
+
+        //These are the flavours of the chicken platters that are possible. There can only be one per platter
+        public enum flavours
+        {
+            Peri_Peri,
+            Mild_Peri_Peri,
+            Lemon_and_Herb
+        }
+        //This is the way the meat will be cooked. They will be selected the same way as flavours
+        public enum prepQuality
+        {
+            rare,
+            medium,
+            medium_well,
+            well_done
+        }
+        //These can be bought extra but with platters you have them for free, so this is a necessary property to use
+        public enum sauces
+        {
+            Lemon_Butter,
+            Lemon_and_Garlic,
+            Peri_Peri
+        }
+       
+    }
+}
