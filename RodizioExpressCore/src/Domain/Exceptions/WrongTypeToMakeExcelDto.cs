42495f118643328d379f0601c0579d046ce84316
Abel3047 <apexmachine2@gmail.com>
2022-05-13 18:30:14 +0200

    Added a new exception that was made in QRCashless

---
+++ RodizioExpressCore/src/Domain/Exceptions/WrongTypeToMakeExcelDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// This is to be thrown if you don't give the ExcelController.CreateExportToExcelDto() the expected object.
+    /// This object is taken from the dashservice after it arrives from the ReportController
+    /// </summary>
+    [Serializable]
+    public class WrongTypeToMakeExcelDto : Exception
+    {
+        public WrongTypeToMakeExcelDto() { }
+        public WrongTypeToMakeExcelDto(string message) : base(message) { }
+        public WrongTypeToMakeExcelDto(string message, Exception inner) : base(message, inner) { }
+        protected WrongTypeToMakeExcelDto(
+          SerializationInfo info,
+          StreamingContext context) : base(info, context) { }
+    }
+}
