bcf43b9fa2be80f9f7ba0dc82e18f6813a8c0180
Abel3047 <apexmachine2@gmail.com>
2022-06-17 17:06:53 +0200

    Final stretches before I close up

--- API/Interfaces/IFirebaseServices.cs
+++ API/Interfaces/IFirebaseServices.cs
@@ -26,22 +26,23 @@ namespace API.Interfaces
         /// <summary>
         /// This takes in the path of the node in the database and coughs up the a list of the type
         /// <typeparamref name="T"/>. <typeparamref name="T"/> is the object type you want to have a list of.
-        /// <para> NOTE: I didnt put type checking here cause there is data that isn't an entity that could be taken from the database, Like flavour</para>
+        /// <para> NOTE: I didnt put type checking here cause there is data that doesn't inherit from <see cref="BaseEntity"/> that could be taken from the database, Like <see cref="Flavour"/></para>
         /// </summary>
         /// <typeparam name="T"> </typeparam>
         /// <param name="path"></param>
         /// <returns> <see cref="List{BaseEntity}"/></returns>
-        /// <remarks> Within the method is logic that tries to change the object to a JObject. Note that you cant try for both JArray and JObject</remarks>
+        /// <remarks> Within the method is logic that tries to change the response to a JObject. Note that you cant try the response for both JArray and JObject</remarks>
         public Task<List<T>> GetData<T>(string path) where T : class, new();
         /// <summary>
-        /// This is used when you are collecting aggreagates from the database. It won't work if you are just trying to get a list of Appusers, but it should work if you are trying to get a list of a list of orderItems, ie an Order
-        /// <para> In case you still don't understand an aggregate is a list of Entities</para>
+        /// This is used when you are collecting aggreagates from the database. For example, it won't work if you are just trying to get a list of <see cref="AppUser"/>s, but it should work if you are trying to get a list of a list of orderItems, ie an <see cref="Order"/>
+        /// <para> In case you still don't understand an aggregate is a list of Entities. Check <see cref="BaseAggregates{T}"/> for more clues. If you find yourself forced to get a <see cref="JArray"/> response ( searching for a list of a list of anything), you should make an Aggregate type
+        /// of the item you are searching for. So that it can 'contain' it</para>
         /// </summary>
         /// <typeparam name="Aggregate"></typeparam>
         /// <typeparam name="Entity"></typeparam>
         /// <param name="path"></param>
         /// <returns></returns>
-        /// <remarks> Within the method is logic that tries to change the object to a JArray. Note that you cant try for both JArray and JObject</remarks>
+        /// <remarks> Within the method is logic that tries to change the response to a JArray. Note that you cant try the response for both JArray and JObject</remarks>
         public Task<List<Aggregate>> GetDataArray<Aggregate, Entity>(string path) where Aggregate : BaseAggregates<Entity>, new();
 
     }
