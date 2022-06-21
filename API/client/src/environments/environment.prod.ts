2f158789853019f10752cda51030aa0fe579707d
Abel3047 <apexmachine2@gmail.com>
2022-06-21 10:51:45 +0200

    I added environment cause there isn't anything sensitive there

---
+++ API/client/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true
+};
+/*
+I'm assuming this is where we will have our environment variables for the client side. But the environment pro.ts isn't in the gitignore,
+I feel like it really should
+*/
