cf900f5d2ac978428488796cdc43e8ae86184455
Abel3047 <apexmachine2@gmail.com>
2022-08-21 11:55:59 +0200

    The same was done to Menu. I look forward to introducing an Aggregate property so that we can remove the private methods that are basically the same code over and over again

--- API/Entities/Aggregates/Menu.cs
+++ API/Entities/Aggregates/Menu.cs
@@ -10,5 +10,41 @@ namespace API.Entities.Aggregates
     /// </summary>
     public class Menu:BaseAggregates<MenuItem>
     {
+        public override string ToString()
+        {
+            string orderItems = null;
+            foreach (MenuItem menuItem in this)
+            {
+                orderItems = menuItem.Name + " IdentityFied with " + menuItem.Id.ToString() + "\n" + " Costing:" + Price.ToString();
+            }
+            return orderItems;
+        }
+
+        // TODO: Have this use the baseAggregate property AggreProp for more conciseness
+        private float? _price
+        {
+            get { return _price == null ? throw new NullReferenceException("There are no OrderItems in this Order") : _price; }
+            set { _price = value; }
+        }
+        /// <summary>
+        /// This is the Price gotten from the adding all the prices of the <see cref="OrderItem"/>s it has. If it doesn't have an element it throws <see cref="NullReferenceException()"/>
+        /// <para> I also made it Immutable/ReadOnly</para>
+        /// </summary>
+        public float? Price
+        {
+            get { return _price; }
+            init
+            {
+                if (this.Any())
+                {
+                    foreach (MenuItem item in this)
+                    {
+                        _price = +float.Parse(item.Price);
+                    }
+                }
+            }
+        }
+
+
     }
 }
