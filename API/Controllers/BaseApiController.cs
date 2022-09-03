fef3c88cb774099be20285a4b3fd3ebffb5f40e2
Abel3047 <apexmachine2@gmail.com>
2022-09-03 14:30:14 +0200

    clean up from last commit
    -forgot to set the protection level of a property to something all Controllers and access

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -25,7 +25,7 @@ namespace API.Controllers
 
         protected readonly IFirebaseServices _firebaseServices;
         static string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        static readonly IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: true).Build();
+        protected static readonly IConfiguration Configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: true).Build();
         protected static readonly HttpClient client = new HttpClient();
 
         public BaseApiController(IFirebaseServices firebaseServices)
