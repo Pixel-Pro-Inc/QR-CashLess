9c4b9b7844457cf32360f9beb58993b0f3b5e00e
Abel3047 <apexmachine2@gmail.com>
2022-10-13 21:07:42 +0200

    clean ups

--- API/Infrastructure/Controllers/OrderController.cs
+++ API/Infrastructure/Controllers/OrderController.cs
@@ -16,52 +16,11 @@ namespace API.Infrastructure.Controllers
 
         public OrderController(IFirebaseServices firebaseServices):base(firebaseServices) { }
 
-        #region Create Order Item
-
         [HttpPost("createorder/{branchId}")]
         public async Task<ActionResult<Order>> CreateOrder(Order orderItems, string branchId)
         {
-            return (await new OrderFactory(branchId,_firebaseServices,notification).MakeOrder(orderItems,"Online")).AddPhoneNumber(orderItems[0].PhoneNumber);
+            return (await new OrderFactory(branchId, _firebaseServices, notification).MakeOrder(orderItems, "Online")).AddPhoneNumber(orderItems[0].PhoneNumber);
         }
-        public async Task<int> GetOrderNum(string branchId)
-        {
-            List<OrderNumber> ordersNumbersResult = await _firebaseServices.GetData<OrderNumber>("AssignedOrderNumbers/" + branchId);
 
-            List<string> oNumbersAssigned = new List<string>();
-
-            if (ordersNumbersResult.Count > 0)
-                oNumbersAssigned = ordersNumbersResult[0].OrderNumbers;
-
-            string d = DateTime.Now.Day.ToString("00") + "-" + DateTime.Now.Month.ToString("00") + "-"
-                    + DateTime.Now.Year.ToString("0000");
-
-            bool clearList = false;
-
-            foreach (var oNumber in oNumbersAssigned)
-            {
-                if(oNumber.Substring(0, 10) != d)
-                {
-                    clearList = true;
-                }
-            }
-
-            if(clearList)
-                oNumbersAssigned.Clear();
-
-            int candidateNumber = new Random().Next(1000, 9999);
-
-            //Check Against Other Order Numbers For The Day
-            while (oNumbersAssigned.Contains(d + "_" + candidateNumber))
-            {
-                candidateNumber = new Random().Next(1000, 9999);
-            }
-
-            oNumbersAssigned.Add(d + "_" + candidateNumber);
-
-            _firebaseServices.StoreData("AssignedOrderNumbers/" + branchId + "/0", new OrderNumber() { OrderNumbers = oNumbersAssigned});
-
-            return candidateNumber;
-        }
-        #endregion
     }
 }
